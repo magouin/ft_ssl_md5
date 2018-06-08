9021c11a2cf98922eb60adc6dfee04020f45465d
jcamhi <jeremshy@h3lp.fr>
2018-06-08 23:48:02 +0200

    Automatic commit

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -5,7 +5,21 @@
 # include <fcntl.h>
 # include <unistd.h>
 
-void print_memory(char *start, size_t size);
+# define CSHIFT(nbr, s) (int)(((nbr) << (s)) | (((nbr) & ((-1u) << (32 - (s)))) >> (32 - (s))))
 
+typedef struct s_params
+{
+	int buffer[4];
+	int k;
+	int s;
+	int i;
+	unsigned int t[65];
+	int x[16];
+}		t_params;
+
+typedef int	(*t_fghi) (int, int, int);
+typedef void (*t_f1234) (t_params*, t_fghi);
+
+void print_memory(char *start, size_t size);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,6 @@
 #include <ft_ssl.h>
 #include <stdio.h>
+#include <math.h>
 
 void	*memjoin_af1(void *m1, size_t s1, const void *m2, size_t s2)
 {
@@ -51,11 +52,144 @@ void    *read_file(char *filename, size_t *file_size)
 	return (ret);
 }
 
+int	f(int x, int y, int z)
+{
+	 return ((x & y) | ((!x) & z));
+}
+
+int	g(int x, int y, int z)
+{
+	return ((x & z) | (y & (!z)));
+}
+
+int	h(int x, int y, int z)
+{
+	return (x ^ y ^ z);
+}
+
+int	i(int x, int y, int z)
+{
+	return (y ^ (x | (!z)));
+}
+
+
+void	initialize_buffer (int *buffer)
+{
+	buffer[0] = 0x67452301;
+	buffer[1] = 0xefcdab89;
+	buffer[2] = 0x98badcfe;
+	buffer[3] = 0x10325476;
+}
+
+void	initialize_t (unsigned int *t)
+{
+	int i;
+
+	i = 1;
+	while (i <= 64)
+	{
+		t[i] = 4294967296.0 * fabs(sin(i));
+		i++;
+	}
+}
+
+void	functions (t_params *params, t_fghi function, int n) //ABCD
+{
+	int	*a;
+	int	*b;
+	int	*c;
+	int	*d;
+
+	a = params->buffer + (0 + n) % 4;
+	b = params->buffer + (1 + n) % 4;
+	c = params->buffer + (2 + n) % 4;
+	d = params->buffer + (3 + n) % 4;
+
+	*a = *b + CSHIFT(((*a + function(*b, *c, *d)
+		+ (params->x)[params->k] + params->t[params->i])), params->s);
+}
+
+// M[] = words du message
+
+void	stage1(t_params *params, char nbr_du_milieu[4][4])
+{
+	int	i;
+
+	i = 0;
+	while (i < 16)
+	{
+		params->k = i;
+		params->s = nbr_du_milieu[0][i % 4];
+		params->i = i + 1;
+		functions(params, f, i % 4);
+		i++;
+	}
+}
+
+void	stage2(t_params *params, char nbr_du_milieu[4][4])
+{
+	int	i;
+
+	i = 0;
+	params->k = 12; // 12 + 5 % 16 = 1
+	while (i < 16)
+	{
+		params->k = (params->k + 5) % 16;
+		params->s = nbr_du_milieu[1][i % 4];
+		(params->i)++;
+		functions(params, g, i % 4);
+		i++;
+	}
+}
+
+void	stage3(t_params *params, char nbr_du_milieu[4][4])
+{
+	int	i;
+
+	i = 0;
+	params->k = 2; // 2 + 3 % 16 = 5
+	while (i < 16)
+	{
+		params->k = (params->k + 3) % 16;
+		params->s = nbr_du_milieu[2][i % 4];
+		(params->i)++;
+		functions(params, h, i % 4);
+		i++;
+	}
+}
+
+void	stage4(t_params *params, char nbr_du_milieu[4][4])
+{
+	int	j;
+
+	j = 0;
+	params->k = 9; // 9 + 7 % 16 = 0
+	while (j < 16)
+	{
+		params->k = (params->k + 7) % 16;
+		params->s = nbr_du_milieu[3][j % 4];
+		(params->i)++;
+		functions(params, i, j % 4);
+		j++;
+	}
+}
+
+
 char	*compute_md5(void *original_file, int64_t original_file_size)
 {
 	int64_t	size;
 	void		*file;
 	int		i;
+	int		j;
+	int		buffer_save[4]; // AA BB CC DD
+	int		stage;
+	int		x;
+	int		y;
+	char nbr_du_milieu [4][4];
+	t_params	params;
+
+
+	ft_memcpy(nbr_du_milieu, (char[4][4]){{7,12,17,22},{5,9,14,20},{4,11,16,23},{6,10,15,21}}, sizeof(nbr_du_milieu));
 
 	size = (original_file_size / 64 + 1) * 64;
 	if (size - original_file_size <= 8)
@@ -73,7 +207,30 @@ char	*compute_md5(void *original_file, int64_t original_file_size)
 		i++;
 	}
 	*(int64_t*)(file + original_file_size + 1 + i) = original_file_size;
-	print_memory(file, size);
+	// print_memory(file, size);
+	initialize_buffer(params.buffer);
+	initialize_t(params.t);
+	i = 0;
+	stage = 0;
+	x = 0;
+	y = 0;
+	while (i <= size / 64 - 1)
+	{
+		j = 0;
+		while (j <= 15)
+		{
+			(params.x)[j] = ((int*)file)[i * 16 + j];
+			j++;
+		}
+		ft_memcpy(buffer_save, params.buffer, sizeof(params.buffer));
+		stage1(&params, nbr_du_milieu);
+		stage2(&params, nbr_du_milieu);
+		stage3(&params, nbr_du_milieu);
+		stage4(&params, nbr_du_milieu);
+		i++;
+	}
+	printf("%x%x%x%x\n", params.buffer[0], params.buffer[1], params.buffer[2], params.buffer[3]);
+	return file;
 }
 
 int main(int argc, char **argv)
@@ -89,5 +246,8 @@ int main(int argc, char **argv)
 	if (!(file = read_file(argv[1], &file_size)))
 		return (2);
 	// write(1, file, file_size);
+
+	printf("%d\n", CSHIFT(3221225472u,2));
 	compute_md5(file, file_size);
 }
+//
